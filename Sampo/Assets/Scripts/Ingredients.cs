150b0dd8121c38ef810153500476bb8cb5523345
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2017-04-11 11:26:35 +0300

    a good sound system

--- Sampo/Assets/Scripts/Ingredients.cs
+++ Sampo/Assets/Scripts/Ingredients.cs
@@ -12,15 +12,55 @@ public class Ingredients : MonoBehaviour {
 	public Button wool;
 	public Button failures;
 	public Button smith;
+	private AudioClip[] milkA;
+	private AudioClip[] milkB;
+	private AudioClip[] barleyA;
+	private AudioClip[] barleyB;
+	private AudioClip[] woolA;
+	private AudioClip[] woolB;
+	private AudioClip[] featherA;
+	private AudioClip[] featherB;
+	private Dictionary<Ingredient, AudioClip[][]> ingredientToSound;
 	private int failuresCount = 0;
+	private int index = 0;
 	private Queue<Ingredient> ingredientQueue;
+	private List<Ingredient> ingredientSet;
 	private PointerEventData pointer = new PointerEventData(EventSystem.current);
-	public GameObject smithEFX;
+	private Ingredient[] correctOrder = {Ingredient.Feather, Ingredient.Milk, Ingredient.Barley, Ingredient.Wool, Ingredient.Failures};
+	private GameObject smithEFX;
+	private AudioSource sound;
+	private bool rhythm = false;
 
 	// Use this for initialization
 	void Start () {
 		ingredientQueue = new Queue<Ingredient> ();
+		ingredientSet = new List<Ingredient> ();
 		smithEFX = GameObject.Find ("FireComplex");
+		milkA = Resources.LoadAll<AudioClip>("Audio/Ingredients/Milk/A");
+		milkB = Resources.LoadAll<AudioClip>("Audio/Ingredients/Milk/B");
+		barleyA = Resources.LoadAll<AudioClip>("Audio/Ingredients/Barley/A");
+		barleyB = Resources.LoadAll<AudioClip>("Audio/Ingredients/Barley/B");
+		woolA = Resources.LoadAll<AudioClip>("Audio/Ingredients/Wool/A");
+		woolB = Resources.LoadAll<AudioClip>("Audio/Ingredients/Wool/B");
+		featherA = Resources.LoadAll<AudioClip>("Audio/Ingredients/Feather/A");
+		featherB = Resources.LoadAll<AudioClip>("Audio/Ingredients/Feather/B");
+		sound = GetComponent<AudioSource> ();
+		ingredientToSound = new Dictionary<Ingredient, AudioClip[][]> ();
+		ingredientToSound.Add(Ingredient.Milk, new AudioClip[][]{milkA, milkB});
+		ingredientToSound.Add(Ingredient.Barley, new AudioClip[][]{barleyA, barleyB});
+		ingredientToSound.Add(Ingredient.Feather, new AudioClip[][]{featherA, featherB});
+		ingredientToSound.Add(Ingredient.Wool, new AudioClip[][]{woolA, woolB});
+	}
+
+	void PlaySound(Ingredient ingredient){
+		AudioClip[] clips = ingredientToSound [ingredient][index];
+
+		sound.PlayOneShot (clips [Random.Range(0, clips.Length)]);
+	
+	}
+		
+	void PlayEFX(){
+		smithEFX.GetComponent<Animator> ().Play ("FireComplexBurst");
 	}
 	
 	// Update is called once per frame
@@ -45,8 +85,14 @@ public class Ingredients : MonoBehaviour {
 			ExecuteEvents.Execute(smith.gameObject, pointer, ExecuteEvents.submitHandler);
 		}
 	}
-		
+
 	void AddIngredient (Ingredient ingredient){
+		// check timer to keep rhytm
+		index = ((index + 1) % 2);
+		PlaySound (ingredient);
+		if(!ingredientSet.Contains(ingredient)){
+			ingredientSet.Add(ingredient);
+		}
 		ingredientQueue.Enqueue (ingredient);
 	}
 
@@ -54,10 +100,22 @@ public class Ingredients : MonoBehaviour {
 		Queue<Ingredient> tempIngredientQueue = ingredientQueue;
 		//print ("Queue before checks: " + ingredientQueue.Count);
 
+		if (rhythm) {
+			print ("Rhythm correct");
+		} else {
+			print ("Rhythm missed");
+		}
+
 		if (AllIngredients ()) {
 			print ("All ingredients");
 		} else {
 			print ("Something missing");
+		}
+
+		if (CheckOrder ()) {
+			print ("Correct order");
+		} else {
+			print ("Incorrect order");
 		}
 
 		if (CheckRepeats ()) {
@@ -67,18 +125,28 @@ public class Ingredients : MonoBehaviour {
 		}
 		//print ("Queue after schecks: " + ingredientQueue.Count);
 
+		ingredientSet.Clear ();
 		ingredientQueue.Clear ();
 	}
 
-	void PlayEFX(){
-		smithEFX.GetComponent<Animator> ().Play ("FireComplexBurst");
+	bool CheckOrder(){
+		for (int i = 0; i < ingredientSet.Count; i++) {
+			if (ingredientSet [i] != correctOrder [i]) {
+				return false;
+			}
+		}
+		if (ingredientSet.Count > 3) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	bool AllIngredients () {
 		if (ingredientQueue.Contains (Ingredient.Milk)
-		   && ingredientQueue.Contains (Ingredient.Wool)
-		   && ingredientQueue.Contains (Ingredient.Barley)
-		   && ingredientQueue.Contains (Ingredient.Feather)) {
+			&& ingredientQueue.Contains (Ingredient.Wool)
+			&& ingredientQueue.Contains (Ingredient.Barley)
+			&& ingredientQueue.Contains (Ingredient.Feather)) {
 			if (failuresCount > 3) {
 				if (ingredientQueue.Contains (Ingredient.Failures)) {
 					return true;
